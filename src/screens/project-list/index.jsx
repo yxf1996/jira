0df6fc2e709e8260f1a7b57448fd9938135961ef
Travis <656379598@qq.com>
2023-09-06 23:32:54 +0800

    ci add commitlint

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import { SearchPanel } from "./searchPanel"
+import { List } from "./list"
+import { cleanObject } from "utils"
+import qs from "qs"
+
+const apiUrl=process.env.REACT_APP_API_URL
+export const  ProjectListScreen = ()=> {
+    const [param,setParam]=useState({
+        name:"",
+        personId:""
+    })
+    const [users,setUsers]=useState([])
+    const [list,setList]=useState([])
+    useEffect(()=>{
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=> {
+            if (response.ok) {
+                setList(await response.json())
+            }
+        })
+    }, [param])
+    useEffect(()=>{
+        fetch(`${apiUrl}/users`).then(async response=> {
+            if (response.ok) {
+                setUsers(await response.json())
+            }
+        })
+    }, [])
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam}></SearchPanel>
+        <List users={users} list={list}></List>
+    </div>
+}
