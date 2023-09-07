8f1c78c4f28df8dbff3f722a15e65904340b2a26
Travis <656379598@qq.com>
2023-09-07 08:52:28 +0800

    <feat> add useMount and useDebounce hooK

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useState, useEffect } from "react"
 import { SearchPanel } from "./searchPanel"
 import { List } from "./list"
-import { cleanObject } from "utils"
+import { cleanObject, useDebounce, useMount } from "utils"
 import qs from "qs"
 
 const apiUrl=process.env.REACT_APP_API_URL
@@ -13,13 +13,14 @@ export const  ProjectListScreen = ()=> {
     })
     const [users,setUsers]=useState([])
     const [list,setList]=useState([])
+    const debounceParam=useDebounce(param,1000)
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=> {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response=> {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debounceParam])
     useEffect(()=>{
         fetch(`${apiUrl}/users`).then(async response=> {
             if (response.ok) {
